e785f0fc45f52a6d00150743b19f79960d818d9b
Apersant1 <miracles.on.the.turn@gmail.com>
2021-05-02 19:31:05 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './Logo/Logo';
+import  '../../styles/App.css';
+const App = () => {
+    return (
+        <>
+        <Logo/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Logo = () => {
+  return (
+    <>
+      {""}
+      <h1>ARA</h1>
+      <p style={{fontFamily:"Magneto"}}>Will a simple</p>
+    </>
+  );
+};
+
+export default Logo;
